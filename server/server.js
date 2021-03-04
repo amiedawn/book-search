e4b1760f0694669e2d7edae59db02507c94ef1c7
amiedawn <amie.d.carroll@gmail.com>
2021-03-04 11:16:56 -0700

    server side done

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require("./config/connection");
+
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
-const db = require('./config/connection');
+
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs, resolvers, context: authMiddleware
+});
 
-app.use(express.urlencoded({ extended: true }));
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -18,4 +35,7 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+
