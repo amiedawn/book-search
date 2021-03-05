a02893464ab7a945bd2b4058d9c695e0ff2ab6a7
amiedawn <amie.d.carroll@gmail.com>
2021-03-04 21:09:47 -0700

    edit savedbooks

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,10 +8,10 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String!); Auth
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: bookInput): User
-    removeBook(bookId: ID!): User
+    removeBook(bookId: String!): User
   }
 
   type User {
