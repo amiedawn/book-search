88b10c129fd73a3a6eeec3c3cc112a54bdfe1799
amiedawn <amie.d.carroll@gmail.com>
2021-03-06 12:27:27 -0700

    revise env

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+// require env file
+require('dotenv').config();
 const express = require('express');
 
 // import ApolloServer
@@ -6,8 +8,6 @@ const { ApolloServer } = require('apollo-server-express');
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require("./config/connection");
-// require env file
-require('dotenv').config();
 
 const { authMiddleware } = require('./utils/auth');
 const path = require('path');
@@ -26,8 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-//if (process.env.NODE_ENV === 'production') {
-if (process.env.JWT_KEY === 'production') {  
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
